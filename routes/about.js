cf03a405fd0bbd0248e00371f5b294b83053a936
Song Malisa Se <sesongmaliisa@gmail.com>
2022-10-05 18:07:57 -0400

    a little spelling again

--- routes/about.js
+++ routes/about.js
@@ -3,17 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Home' });
+  res.render('home');
 });
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('home', { title: 'Home' });
+  res.render('home');
 });
 
 /* GET About Us page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About' });
+  res.render('about');
 });
 
 /* GET Products page. */
